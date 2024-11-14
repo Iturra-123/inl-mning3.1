a1f17e820a71caa0d3932fce35afd00520fa7621
leit07001 <leit07001@E-PW05C37S>
2024-11-14 12:13:12 +0100

    Add project files.

---
+++ tävling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.Design;
+namespace tävling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >= 16 && ålder <= 19)
+                Console.WriteLine("Du får vara med och delta i tävlingen");
+            else if (ålder < 16)
+                Console.WriteLine("Du är för ung för att delta");
+            
+            else Console.WriteLine(" Du är för gammal för att delta");
+        }
+    }
+}
